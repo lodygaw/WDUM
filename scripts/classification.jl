8a69bf310bc7c44e68c552b999b9c8d58e20c820
lodygaw <wiklod.spam@gmail.com>
2023-01-15 22:55:14 +0100

    Minor refactor

--- scripts/classification.jl
+++ scripts/classification.jl
@@ -8,7 +8,8 @@ using ScikitLearn: @sk_import
 @sk_import preprocessing : StandardScaler
 @sk_import pipeline : make_pipeline
 
-download_dataset("Cricket")
+# Download datasets if needed
+# download_dataset("Cricket")
 
 X_train, y_train, X_test, y_test = load_dataset("Cricket");
 
@@ -21,17 +22,31 @@ X_test = trim_timeseries(X_test, y_test, ranges)
 
 # show_stratification(X_train, y_train)
 
+# Forecast trimmed timeseries with 3 different algorithm
+
 target_length = 1000
 
-X_train_forecasted_org = forecast(NaiveSingle, X_train, target_length, 4, 3)
-X_test_forecasted_org = forecast(NaiveSingle, X_test, target_length, 4, 3)
-
-X_train_forecasted_mult = forecast(NaiveMultiple, X_train, target_length, 100, 20, 0.4f0)
-X_test_forecasted_mult = forecast(NaiveMultiple, X_test, target_length, 100, 20, 0.4f0)
-
+# forecast with mean padding
 X_train_forecasted_mean = forecast(Mean, X_train, target_length)
 X_test_forecasted_mean = forecast(Mean, X_test, target_length)
 
+# forecast with NaiveMultiple original algorithm
+max_overlap = ceil(Int, 0.1 * target_length)
+min_overlap = ceil(Int, 0.01 * target_length)
+threshold = Float32(0.4)
+
+X_train_forecasted_mult = forecast(NaiveMultiple, X_train, target_length, max_overlap, min_overlap, threshold)
+X_test_forecasted_mult = forecast(NaiveMultiple, X_test, target_length, max_overlap, min_overlap, threshold)
+
+# forecast with NaiveSingle original algorithm
+window_size = 4
+overlap = 3
+
+X_train_forecasted_org = forecast(NaiveSingle, X_train, target_length, window_size, overlap)
+X_test_forecasted_org = forecast(NaiveSingle, X_test, target_length, window_size, overlap)
+
+
+# Transform with ROCKET
 rocket_org = Rocket()
 rocket_mean = Rocket()
 rocket_mult = Rocket()
@@ -40,13 +55,13 @@ fit!(rocket_org, X_train_forecasted_org);
 fit!(rocket_mean, X_train_forecasted_mean);
 fit!(rocket_mult, X_train_forecasted_mean);
 
-X_train_transform_org = transform!(rocket_org, X_train_forecasted_org)
-X_train_transform_mean = transform!(rocket_mean, X_train_forecasted_mean)
-X_train_transform_mult = transform!(rocket_mult, X_train_forecasted_mult)
+X_train_transform_org = transform!(rocket_org, X_train_forecasted_org);
+X_train_transform_mean = transform!(rocket_mean, X_train_forecasted_mean);
+X_train_transform_mult = transform!(rocket_mult, X_train_forecasted_mult);
 
-X_test_transform_org = transform!(rocket_org, X_test_forecasted_org)
-X_test_transform_mean = transform!(rocket_mean, X_test_forecasted_mean)
-X_test_transform_mult = transform!(rocket_mult, X_test_forecasted_mult)
+X_test_transform_org = transform!(rocket_org, X_test_forecasted_org);
+X_test_transform_mean = transform!(rocket_mean, X_test_forecasted_mean);
+X_test_transform_mult = transform!(rocket_mult, X_test_forecasted_mult);
 
 alphas = [1.00000000e-03, 4.64158883e-03, 2.15443469e-02, 1.00000000e-01,
        4.64158883e-01, 2.15443469e+00, 1.00000000e+01, 4.64158883e+01,
